bd950bfc99767fc17bf9cb425801b3808703c4e0
Deaglusha <deaglusha@gmail.com>
2022-07-05 22:59:41 +0300

    Change Exercise1

--- src/main/java/Exercise1.java
+++ src/main/java/Exercise1.java
@@ -15,7 +15,7 @@ public class Exercise1 {
     public static String getString(List<String> input) {
         return IntStream.range(0, input.size())
                 .filter(n -> n % 2 != 0)
-                .mapToObj(index -> index + ". " + input.get(index))
+                .mapToObj(index -> String.format("%d. %s", index, input.get(index - 1)))
                 .collect(Collectors.joining(", "));
     }
 }
